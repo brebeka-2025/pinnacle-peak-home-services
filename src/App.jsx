e9cfff22f53658307b639c0b9a50642259b363fb
brebeka-2025 <bob.rebeka@gmail.com>
2026-02-28 21:09:13 -0700

    Fix scroll to top on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
 import Services from './pages/Services'
@@ -8,17 +9,25 @@ import TheWatch from './pages/TheWatch'
 import ArticlePage from './pages/ArticlePage'
 import Contact from './pages/Contact'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="services" element={<Services />} />
-        <Route path="about" element={<About />} />
-        <Route path="faq" element={<FAQ />} />
-        <Route path="the-watch" element={<TheWatch />} />
-        <Route path="the-watch/:slug" element={<ArticlePage />} />
-        <Route path="contact" element={<Contact />} />
+        <Route index element={<><ScrollToTop /><Home /></>} />
+        <Route path="services" element={<><ScrollToTop /><Services /></>} />
+        <Route path="about" element={<><ScrollToTop /><About /></>} />
+        <Route path="faq" element={<><ScrollToTop /><FAQ /></>} />
+        <Route path="the-watch" element={<><ScrollToTop /><TheWatch /></>} />
+        <Route path="the-watch/:slug" element={<><ScrollToTop /><ArticlePage /></>} />
+        <Route path="contact" element={<><ScrollToTop /><Contact /></>} />
       </Route>
     </Routes>
   )
